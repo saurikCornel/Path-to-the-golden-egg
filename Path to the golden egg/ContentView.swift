2c7a4d040030aa14af47308f460346b455f096d0
Alex Belov <alex@cornel.su>
2025-06-06 20:59:58 +0300

    Initial Commit

---
+++ Path to the golden egg/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Path to the golden egg
+//
+//  Created by alex on 6/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Path to the golden egg/Path_to_the_golden_eggApp.swift
@@ -0,0 +1,17 @@
+//
+//  Path_to_the_golden_eggApp.swift
+//  Path to the golden egg
+//
+//  Created by alex on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Path_to_the_golden_eggApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
